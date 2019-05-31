33f919e0e680a34aeb79b1fd840efd964493d564
max keller <max.keller427@gmail.com>
2019-05-30 18:34:39 -0600

    almost done

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const db = require("./data/dbConfig");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/api/cohorts", async (req, res) => {
+  try {
+    const cohorts = await db("cohorts");
+    res.status(200).json(cohorts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+server.get("/api/cohorts/:id", async (req, res) => {
+  try {
+    const cohortId = await db("cohorts")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(cohortId);
+  } catch (err) {
+    res.status(500).json({ message: "Couldnt Find Cohort" });
+  }
+});
+
+// server.get('/api/cohorts/:id/students', async (req, res) => {
+//   try {
+
+//   }
+// })
+
+server.post("/api/cohorts", async (req, res) => {
+  try {
+    const [id] = await db("cohorts").insert(req.body);
+    const cohort = await db("cohorts")
+      .where({ id })
+      .first();
+    res.status(200).json(cohort);
+  } catch (err) {
+    res.status(500).json({ message: "Couldnt Add Cohort" });
+  }
+});
+
+server.put("/api/cohorts/:id", async (req, res) => {
+  try {
+    const count = await db("cohorts")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const cohort = await db("cohorts")
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(cohort);
+    } else {
+      res.status(404).json({ message: "Cohort does not exist" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error adding Cohort" });
+  }
+});
+
+server.delete("/api/cohorts/:id", async (req, res) => {
+  try {
+    const count = await db("cohorts")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      const cohort = await db("cohorts")
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json({ message: "Cohort Deleted" });
+    } else {
+      res.status(404).json({ message: "Cohort does not exist" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting Cohort" });
+  }
+});
+
+server.listen(4000, () => {
+  console.log(`Server Running On Port 4000`);
+});
---
+++ seeds/001-cohorts.js
@@ -0,0 +1,9 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("cohorts")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("cohorts").insert([{ name: "webpt4" }, { name: "webpt3" }]);
+    });
+};
